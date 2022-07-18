24c61eff68fd2b52862b68ea7cf9a46f048f1f44
unknown <aabhasjain96@gmail.com>
2022-07-18 16:59:16 +0530

    Changed data and some CSS

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,53 +1,57 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
-import {FaUserGraduate} from 'react-icons/fa'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import { FaUserGraduate } from "react-icons/fa";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5> Get to Know</h5>
       <h2>About Me</h2>
 
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt='About Image'/>
+            <img src={ME} alt="About Image" />
           </div>
         </div>
 
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>2+ Years Working</small>
             </article>
 
-            <article className='about__card'>
-              <FaUserGraduate className='about__icon'/>
+            <article className="about__card">
+              <FaUserGraduate className="about__icon" />
               <h5>Post Graduate</h5>
               <small>MCA</small>
             </article>
 
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>80+ completed</small>
             </article>
           </div>
           <p>
-          I’m a web developer. I spend my whole day, practically every day, experimenting with HTML, CSS, and JavaScript; dabbling with Python and Ruby; and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I build websites that delight and inform. I do it well.
+            I’m a web developer. I spend my whole day, practically every day,
+            experimenting with HTML, CSS, and JavaScript; dabbling with React
+            JS. I build websites that delight and inform. I do it well.
           </p>
 
-          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
