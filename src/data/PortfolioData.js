54d6ceeacb3c30dd8f0eb18ca0035ca5861b1f44
unknown <aabhasjain96@gmail.com>
2022-07-19 12:48:54 +0530

    Made Final chnages

--- src/data/PortfolioData.js
+++ src/data/PortfolioData.js
@@ -1,6 +1,6 @@
 
 import IMG1 from "../assets/portfolio1.png";
-import IMG2 from "../assets/portfolio2.jpg";
+import IMG2 from "../assets/portfolio2.png";
 import IMG3 from "../assets/portfolio3.jpg";
 import IMG4 from "../assets/portfolio4.jpg";
 import IMG5 from "../assets/portfolio5.png";
@@ -17,38 +17,38 @@ const data = [
   {
     id: 2,
     image: IMG2,
-    title: "Project Title 2",
-    github: "https://github.com",
-    demo: "https://github.com",
+    title: "Covid-19 Tracker",
+    github: "https://github.com/Aabhas-Jain/covid-19-tracker",
+    demo: "https://covid-19-tracker-9b01a.web.app/",
   },
-  {
-    id: 3,
-    image: IMG3,
-    title: "Project Title 3",
-    github: "https://github.com",
-    demo: "https://github.com",
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: "Project Title 4",
-    github: "https://github.com",
-    demo: "https://github.com",
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: "Project Title 5",
-    github: "https://github.com",
-    demo: "https://github.com",
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: "Project Title 6",
-    github: "https://github.com",
-    demo: "https://github.com",
-  },
+  // {
+  //   id: 3,
+  //   image: IMG3,
+  //   title: "Project Title 3",
+  //   github: "https://github.com",
+  //   demo: "https://github.com",
+  // },
+  // {
+  //   id: 4,
+  //   image: IMG4,
+  //   title: "Project Title 4",
+  //   github: "https://github.com",
+  //   demo: "https://github.com",
+  // },
+  // {
+  //   id: 5,
+  //   image: IMG5,
+  //   title: "Project Title 5",
+  //   github: "https://github.com",
+  //   demo: "https://github.com",
+  // },
+  // {
+  //   id: 6,
+  //   image: IMG6,
+  //   title: "Project Title 6",
+  //   github: "https://github.com",
+  //   demo: "https://github.com",
+  // },
 ];
 
 export default data
